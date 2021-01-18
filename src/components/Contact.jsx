a049d5b165b341e68d6867a28a4849a12fcb9856
Surendra Galwa <surendra.galwa@nymo.ai>
2021-01-18 16:09:23 +0530

    removed resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import userInfo from '../data/User.json';
 import userImage from '../data/background.jpeg';
-import resume from '../data/resume.pdf';
+// import resume from '../data/resume.pdf';
 import mobileIcon from '../data/icons/mobile.png';
 import linkedinIcon from '../data/icons/linkedin.png';
 import hackerrankIcon from '../data/icons/hackerrank.png';
@@ -17,7 +17,7 @@ class Contact extends Component {
     }
 
     render() {
-        return(
+        return (
             <div className='contact-page page'>
                 <h1 className='title'>
                     {userInfo.contact.title}
@@ -27,11 +27,11 @@ class Contact extends Component {
                         <img src={userImage} className='user-image' alt={userInfo.contact.name} />
                         <h3 className='user-name'>{userInfo.contact.name}</h3>
                         <h3 className='call-section'><img src={mobileIcon} alt='mobile' />{userInfo.contact.mobile}</h3>
-                        <h3>
+                        {/* <h3>
                             <a href={resume} download className='active-link'>
                                 {userInfo.contact.resume_label}
                             </a>
-                        </h3>
+                        </h3> */}
                         <div className='social-profile'>
                             <a href={userInfo.contact.linkedin_url} target='_blank' rel="noopener noreferrer">
                                 <img src={linkedinIcon} alt='linkedin' />
